ea84c52165b1bf88b98c304d831a3175d30f1a96
Gui-Ailton <gui_martins16@outlook.com>
2021-09-30 21:11:26 -0300

    Product repository, service, resource, GET ok

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -38,6 +38,7 @@ public class Product implements Serializable {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant date;
 	
+	//Anotação que coloca quando é muitos para muitos.
 	@ManyToMany
 	@JoinTable(name = "tb_product_category",joinColumns = @JoinColumn(name = "product_id"),inverseJoinColumns = @JoinColumn(name = "category_id"))
 	Set<Category> categories = new HashSet<>();
