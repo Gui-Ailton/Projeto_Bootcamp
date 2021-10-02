547156d65b15ded7e83cb197d4e11f7f6b23b8d5
Gui-Ailton <gui_martins16@outlook.com>
2021-10-01 22:01:45 -0300

    Product insert,update,delete

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
@@ -13,17 +12,24 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 
 @Service
 public class ProductService {
-
+	
+	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	//Quando for operação que é apenas leitura, sempre colocar essa anotação, para melhorar performace
 	@Transactional(readOnly = true)
@@ -43,18 +49,20 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		entity.setName(dto.getName());
+		copyDtoToEntity(dto,entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 		
 	}
+
+	
 
 	@Transactional
 	public ProductDTO update(Long id,ProductDTO dto) {
 	
 		try {
 		Product entity = repository.getOne(id);
-		entity.setName(dto.getName());
+		copyDtoToEntity(dto,entity);
 		entity= repository.save(entity);
 		return new ProductDTO(entity);
 		}
@@ -73,5 +81,19 @@ public class ProductService {
 		catch(DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity violation");
 		}
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId()); 
+			entity.getCategories().add(category);
+		}
+		
 	}
 }
